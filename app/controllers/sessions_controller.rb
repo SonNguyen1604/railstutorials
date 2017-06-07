8c42172c3463f69297f870943a4787968a38108c
SonNguyen1604 <sonnguyenngoc1604@gmail.com>
2017-06-07 09:29:32 +0700

    Finish account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,16 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
 
     if user && user.authenticate(params[:session][:password])
-      flash[:success] = t ".success"
-      log_in user
-      "1" == params[:session][:remember_me] ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        "1" == params[:session][:remember_me] ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message = t ".not_activated"
+        message += t ".check_mail"
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = t ".fail"
       render :new
