524376eb056e8b5f47d2e7f9fe504e5d382c4306
SonNguyen1604 <sonnguyenngoc1604@gmail.com>
2017-06-07 09:29:32 +0700

    Finish update, show, delete user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: [:new, :create, :show]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  before_action :load_user, except: [:new, :create, :index]
+
+  def index
+    @users = User.select(:id, :name, :email).id_sort
+      .paginate page: params[:page], per_page: Settings.user.per_page
+  end
+
   def new
     @user = User.new
   end
@@ -17,13 +27,56 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by id: params[:id]
-    render file: "public/404.html", layout: false unless @user
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".updated"
+      redirect_to @user
+    else
+      flash.now[:danger] = t ".fail_update"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t ".deleted"
+    else
+      flash[:danger] = t ".fail_delete"
+    end
+    redirect_to users_url
   end
 
   private
 
+  def load_user
+    @user = User.find_by id: params[:id]
+
+    render file: "public/404.html", layout: false unless @user
+  end
+
   def user_params
     params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t ".login_first"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 end
